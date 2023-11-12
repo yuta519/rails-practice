7d4ea2b598857a3344b9c78c517a5e85f12f1925
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-12 10:47:28 -0800

    Delete previous

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Article < ApplicationRecord
-  has_many :comments
-
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
-end
