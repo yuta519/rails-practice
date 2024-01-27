f880baf43230fa19b67bf1c36b4e947d395cf3d6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-27 11:38:48 -0800

    Add About page

--- apps/sample_app/app/controllers/static_pages_controller.rb
+++ apps/sample_app/app/controllers/static_pages_controller.rb
@@ -2,8 +2,14 @@
 
 class StaticPagesController < ApplicationController
   def home
+    @page_title = 'Home'
   end
 
   def help
+    @page_title = 'Help'
+  end
+
+  def about
+    @page_title = 'About'
   end
 end
