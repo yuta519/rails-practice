9621654c62f85ccd31043d8ab9faa5b447a8941f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-27 14:11:41 -0800

    Create custom helper, full_title

--- apps/sample_app/app/helpers/application_helper.rb
+++ apps/sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# module ApplicationHelper
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
+  end
 end
