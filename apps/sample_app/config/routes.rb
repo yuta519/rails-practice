aeb86476d4dfc7c3195bf83773b7d3822a8359d0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-27 11:40:06 -0800

    Add test to check title

--- apps/sample_app/config/routes.rb
+++ apps/sample_app/config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
