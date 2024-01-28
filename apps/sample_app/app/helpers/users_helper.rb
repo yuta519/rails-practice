fd05efe2e66cff0979239b2738e99e6cb7c8903c
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-28 15:53:53 -0800

    Add show view

--- apps/sample_app/app/helpers/users_helper.rb
+++ apps/sample_app/app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# User Helper
 module UsersHelper
+  def gravatar_for(user, size: 80)
+    gravator_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+    image_tag(gravator_url, alt: user.name, class: 'gravator')
+  end
 end
