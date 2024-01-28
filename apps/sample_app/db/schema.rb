d7fccde945b74f56bf05216b2be8f93250aa321f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-28 07:57:46 -0800

    Create User Model

--- apps/sample_app/db/schema.rb
+++ apps/sample_app/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,5 +12,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 20_240_128_155_045) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 end
