84b327292adb40d1bcff22a1fc3c569248e01d87
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-28 09:03:45 -0800

    Add length validation

--- apps/sample_app/app/models/user.rb
+++ apps/sample_app/app/models/user.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# User model
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
