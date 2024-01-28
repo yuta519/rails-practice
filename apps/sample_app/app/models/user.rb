ebb0bb3b09666cfef8dbe7263b016fd0a8e8319e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-28 10:56:22 -0800

    Add email validation

--- apps/sample_app/app/models/user.rb
+++ apps/sample_app/app/models/user.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+
 # User model
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
