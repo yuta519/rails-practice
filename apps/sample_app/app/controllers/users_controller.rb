59bc410eaf1ae84a019e5e7705126925115f9cd9
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-28 15:54:18 -0800

    Create signup view

--- apps/sample_app/app/controllers/users_controller.rb
+++ apps/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
+# User Controller
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
   end
 end
