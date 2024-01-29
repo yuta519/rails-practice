740f66a1589902afb6bd4f1bea61de641a9a81bc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-29 11:17:08 -0800

    Add test for failed case

--- apps/sample_app/test/integration/users_signup_test.rb
+++ apps/sample_app/test/integration/users_signup_test.rb
@@ -18,4 +18,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
   end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        name: 'Example User',
+        email: 'user@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      } }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_equal 'Welcome to the Sample App!', flash[:success]
+    assert_select 'h1', 'Example User'
+  end
 end
