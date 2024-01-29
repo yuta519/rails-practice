5c02b561eeac12eb9028e65f1a16215cebab3b2a
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-29 15:58:01 -0800

    Create helper function curre_user

--- apps/sample_app/app/helpers/sessions_helper.rb
+++ apps/sample_app/app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
+# Session helper
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
