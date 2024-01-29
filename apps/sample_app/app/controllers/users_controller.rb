750b29752d9de1f667ef5c55ed235a38553bf680
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-29 10:32:47 -0800

    Create user from

--- apps/sample_app/app/controllers/users_controller.rb
+++ apps/sample_app/app/controllers/users_controller.rb
@@ -9,4 +9,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confimation)
+  end
 end
