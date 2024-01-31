473ad5ddfd6a0f63302ba19fde96196c79a3fa0b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-31 11:21:29 -0800

    Use sample data in Prodcution

--- apps/sample_app/bin/render-build.sh
+++ apps/sample_app/bin/render-build.sh
@@ -5,3 +5,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed
