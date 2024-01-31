c9d7e7c7b968841492aa7ef843ac024e574642bb
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-01-30 16:59:28 -0800

    Add authorization for user edit and update

--- apps/sample_app/app/controllers/users_controller.rb
+++ apps/sample_app/app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 # User Controller
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
   def show
     @user = User.find(params[:id])
   end
@@ -41,5 +42,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confimation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
   end
 end
--- apps/sample_app/test/integration/users_edit_test.rb
+++ apps/sample_app/test/integration/users_edit_test.rb
@@ -8,16 +8,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user),
-          params: { user: { name: '', email: 'foo@invd', password: '', password_confirmation: 'baz' } }
+          params: { user: { name: '', email: 'foo@invd', password: 'bo', password_confirmation: 'baz' } }
     assert_template 'users/edit'
 
-    assert_select 'div.alert', 'The form contains 4 errors.'
+    assert_select 'div.alert', 'The form contains 3 errors.'
   end
 
   test 'successful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
 
@@ -30,5 +32,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test 'should redirect edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
